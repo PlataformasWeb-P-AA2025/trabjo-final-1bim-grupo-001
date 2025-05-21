aae89fa06d96b8c2a00c57e7608ae81233d45fa7
Dara <dvan1@utpl.edu.ec>
2025-05-21 09:03:35 -0500

    Generar Tablas 2

--- Generar_tablas.py
+++ Generar_tablas.py
@@ -1,71 +1,46 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey
-
 from configuracion import cadena_base_datos
 
 engine = create_engine(cadena_base_datos)
-
 Base = declarative_base()
-
 
 class usuarios(Base):
     __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
     nombre_usuario = Column(String(500))
-    publicacion_usuario = relationship("Publicacion_usuario", back_populates="usuarioa")
-
+    publicacion = relationship("publicacion", back_populates="usuario")
+    reacciones = relationship("reacciones", back_populates="usuario")
 
     def __repr__(self):
-        return "Usuario: nombre=%s" % (
-                          self.nombre_usuario )
-
+        return "Usuario: nombre=%s" % self.nombre_usuario
 
 class publicacion(Base):
     __tablename__ = 'publicacion'
     id = Column(Integer, primary_key=True)
     publicacion = Column(String(500))
-    publicacion_usuario = relationship("Publicacion_usuario", back_populates="publicaciones")
+    usuario = relationship("usuario", back_populates="publicacion")
+    reacciones = relationship("reacciones", back_populates="publicacion")
 
     def __repr__(self):
-        return "Usuario: nombre=%s" % (
-                          self.publicacion)
-    
+        return "Publicacion: %s" % self.publicacion
 
-
-    
-class publicacion_usuario(Base):
-    __tablename__ = 'publicacion_usuario'
+class reacciones(Base):
+    __tablename__ = 'reaccion'
     id = Column(Integer, primary_key=True)
-    publicacion_id =  Column(Integer, ForeignKey("publicacion.id"),primary_key=True)
-    usuario_id =Column(Integer, ForeignKey("usuario.id"),primary_key=True)
+    publicacion_id = Column(Integer, ForeignKey("publicacion.id"))
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+    tipo_reaccion = Column(String(100))
 
-    publicacion = relationship("Publicacion", back_populates="usuarios")
-    usuario = relationship("Usuarios", back_populates="publicaciones")
-
-    
-
+    publicacion = relationship("publicacion", back_populates="reacciones")
+    usuario = relationship("usuarios", back_populates="reacciones")
 
     def __repr__(self):
-        return "Usuario: nombre=%s \n Publicacion: publicacion=%s \n "%(
-                          self.usuario
-                          self.publicacion,
-                            )
-    
+        return "Usuario: %s \nPublicacion: %s \nTipo_reaccion: %s" % (
+            self.usuario,
+            self.publicacion,
+            self.tipo_reaccion
+        )
 
-class emociones(Base):
-    __tablename__ = 'emocion'
-    id = Column(Integer, primary_key=True)
-    publicacion_id =  Column(Integer, ForeignKey("publicacion.id"),primary_key=True)
-    usuario_id =Column(Integer, ForeignKey("usuario.id"),primary_key=True)
-    tipo_emocion = Column(String(100))
-
-    publicacion = relationship("Publicacion", back_populates="usuarios")
-    usuario = relationship("Usuarios", back_populates="publicaciones")
-
-    def __repr__(self):
-        return "Usuario: nombre=%s \n Publicacion: publicacion=%s \n Tipo_emocion:tipo_emocion =%s \n"%(
-                          self.usuario
-                          self.publicacion,
-                            )
+Base.metadata.create_all(engine)
